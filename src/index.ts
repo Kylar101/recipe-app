75aa6a2ed8d24d3eadf8f21bbcdbf05cedb876bd
Ben Lowbridge <ben.lowbridge@mudbath.com.au>
2020-01-24 18:16:29 +1100

    Changes to imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
 
 // Some fake data
 const books = [
