6400f1278b18d26fe5982b59ce88dd59b51e5cdd
Ben Lowbridge <ben.lowbridge@mudbath.com.au>
2020-02-05 21:08:30 +1100

    Moves to typeorm

--- server/src/graphql/Query/index.js
+++
@@ -1,7 +0,0 @@
-const todo = require('./user')
-const todos = require('./users')
-const Query = {
-  todo,
-  todos,
-}
-module.exports = Query
--- server/src/graphql/index.js
+++
@@ -1,3 +0,0 @@
-const Query = require('./Query')
-const resolvers = { Query }
-module.exports = { resolvers }
